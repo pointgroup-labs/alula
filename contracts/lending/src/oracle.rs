e925fa109ddf1602237eff3c905eb0c9d4678b4a
tiamo <vk.tiamo@gmail.com>
2025-05-15 01:00:18 +0300

    refactor: adjust contract import for testing

--- contracts/lending/src/oracle.rs
+++ contracts/lending/src/oracle.rs
@@ -1,5 +1,7 @@
 use soroban_sdk::contractimport;
 
-contractimport!(
-    file = "../../reflector-contract/target/wasm32-unknown-unknown/release/reflector_oracle.wasm"
-);
+// #[cfg(not(test))]
+contractimport!(file = "../../target/wasm32-unknown-unknown/release/reflector_oracle.wasm");
+
+// #[cfg(test)]
+// contractimport!(file = "../../target/wasm32-unknown-unknown/release/reflector_oracle_mock.wasm");
