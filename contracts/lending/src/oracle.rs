bafbf493e06e1ceca2d6a15efdcc8c9f985e40a4
Maksym Khomiak <khomiakmaxim@gmail.com>
2025-06-02 11:17:42 +0000

    feat: add 'integration_tests' feature

--- contracts/lending/src/oracle.rs
+++ contracts/lending/src/oracle.rs
@@ -1,6 +1,7 @@
 use soroban_sdk::contractimport;
 
-// Integration tests compile the 'lending' crate as a dependency, hence #[cfg(not(test))] is not hit for them.
-// TODO: Automate switching between `reflector_oracle` for deploy build and `reflector_oracle_mock`
-// for integration tests build
+#[cfg(feature = "integration_tests")]
 contractimport!(file = "../../target/wasm32-unknown-unknown/release/reflector_oracle_mock.wasm");
+
+#[cfg(not(feature = "integration_tests"))]
+contractimport!(file = "../../target/wasm32-unknown-unknown/release/reflector_oracle.wasm");
