d494d65242d0ee05b4bd19323c3f6cc28edb24b2
garniy <forbedlam@gmail.com>
2025-07-24 16:30:43 +0300

    fix: app show transition

--- packages/ui/src/App.vue
+++ packages/ui/src/App.vue
@@ -1,3 +1,15 @@
+<script lang="ts" setup>
+onMounted(() => {
+  nextTick(() => {
+    const body = document.querySelector('body') as HTMLElement
+    if (body) {
+      body.style.transition = 'opacity 0.3s ease-in-out'
+      body.style.opacity = '1'
+    }
+  })
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
