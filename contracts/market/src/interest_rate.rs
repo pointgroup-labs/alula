08433da427102fcde27004b692b49453eb6546c3
Maksym Khomiak <khomiakmaxim@gmail.com>
2025-12-22 05:41:57 +0000

    chore: check 'interest_rate.rs'

--- contracts/market/src/interest_rate.rs
+++ contracts/market/src/interest_rate.rs
@@ -58,14 +58,9 @@ impl Pool {
             .fixed_mul_ceil(self.config.fee_config.take_rate_bps as i128, BPS_FACTOR)
             .map_over_or_underflow()?;
 
+        // TODO: Is it OK that we deduce `take_rate` from compounded accrual?
+        // TODO: Is everything OK here with precision loss?
         if take_rate_accrual_part > 0 {
-            // TODO: Check what to do with precision loss &
-            // with cases where no beneficiaries present, yet take_rate is non-zero
-
-            // Regarding the first - we can have an endpoint that allows for an admin
-            // to take all such fees to the market itself, right?
-
-            // Yep, distribute must do so, I think - we simply forget about them?
             if let Some(take_rate_beneficiaries) = &self.config.fee_config.take_rate_beneficiaries {
                 for (beneficiary_addr, share_bps) in take_rate_beneficiaries.iter() {
                     let share = take_rate_accrual_part
@@ -94,7 +89,9 @@ impl Pool {
             .fixed_mul_floor(BPS_FACTOR - self.config.fee_config.take_rate_bps as i128, BPS_FACTOR)
             .map_over_or_underflow()?;
 
-        // -- Accrue supply APR bootstraps --
+        self.last_accrual_timestamp = current_timestamp;
+
+        // -- Accrue supply APR bootstraps(candidate to be removed) --
 
         let mut updated_periods: Vec<((u64, u64), PoolBootstrapPeriod)> = svec![e];
         let mut outdated_periods: Vec<(u64, u64)> = svec![e];
@@ -139,105 +136,6 @@ impl Pool {
 
         Ok(())
     }
-
-    // /// Accrues interest on the pool's total borrowed amount based
-    // /// on the time elapsed since the last accrual
-    // pub fn accrue_interest(&mut self, e: &Env) -> Result<(), MCError> {
-    //     let current_timestamp = e.ledger().timestamp();
-    //     if current_timestamp < self.last_accrual_timestamp {
-    //         events::current_ledger_timestamp_smaller_than_stored_timestamp(
-    //             e,
-    //             current_timestamp,
-    //             self.last_accrual_timestamp,
-    //         );
-
-    //         return Err(MCError::InternalError);
-    //     }
-
-    //     // -- Accrue interest on the pool --
-
-    //     let seconds_passed = current_timestamp - self.last_accrual_timestamp; // safe
-    //     if seconds_passed == 0 {
-    //         // NB: No time passed, no interest to accrue
-    //         return Ok(());
-    //     }
-
-    //     let utilization_ratio_bps = self.compute_utilization_ratio_bps()?;
-    //     let current_borrow_apr =
-    //         self.config.interest_rate_model.compute_borrow_apr(utilization_ratio_bps)?;
-    //     let accrual_multiplier =
-    //         self.config.accrual_model.compute_multiplier(current_borrow_apr, seconds_passed)?;
-
-    //     let new_total_borrowed = self
-    //         .total_borrowed
-    //         .fixed_mul_ceil(accrual_multiplier, SCALED_FIXED_POINT_DENOMINATOR)
-    //         .map_over_or_underflow()?;
-    //     let accrued =
-    //         new_total_borrowed.checked_sub(self.total_borrowed).map_over_or_underflow()?;
-    //     let accrued_to_reserve = accrued
-    //         .fixed_mul_ceil(self.config.fee_config.take_rate_bps as i128, BPS_FACTOR)
-    //         .map_over_or_underflow()?;
-    //     let new_accumulated_reserve_fees = self
-    //         .accumulated_reserve_fees
-    //         .checked_add(accrued_to_reserve)
-    //         .map_over_or_underflow()?;
-
-    //     self.accumulated_reserve_fees = new_accumulated_reserve_fees;
-    //     self.total_borrowed = new_total_borrowed;
-    //     self.last_accrual_timestamp = current_timestamp;
-
-    //     self.borrow_apr_bps = current_borrow_apr;
-    //     self.supply_apr_bps = current_borrow_apr
-    //         .fixed_mul_floor(utilization_ratio_bps, BPS_FACTOR)
-    //         .map_over_or_underflow()?
-    //         .fixed_mul_floor(BPS_FACTOR - self.config.fee_config.take_rate_bps as i128, BPS_FACTOR)
-    //         .map_over_or_underflow()?; // safe
-
-    //     // -- Accrue supply APR bootstraps --
-
-    //     let mut updated_periods: Vec<((u64, u64), PoolBootstrapPeriod)> = svec![e];
-    //     let mut outdated_periods: Vec<(u64, u64)> = svec![e];
-
-    //     for ((start_period, end_period), mut pool_bootstrap_period) in self.bootstrap_periods.iter()
-    //     {
-    //         if end_period <= current_timestamp {
-    //             let new_total_available = self
-    //                 .total_available
-    //                 .checked_add(pool_bootstrap_period.remaining_amount)
-    //                 .map_over_or_underflow()?;
-
-    //             self.total_available = new_total_available;
-    //             outdated_periods.push_back((start_period, end_period));
-    //         } else if current_timestamp > start_period && current_timestamp < end_period {
-    //             let remaining_time_period = end_period - current_timestamp; // safe
-    //             let remaining_time_ratio = remaining_time_period
-    //                 .fixed_div_ceil(end_period - start_period, BPS_FACTOR as u64)
-    //                 .map_over_or_underflow()?; // safe
-
-    //             let new_remaining_amount = pool_bootstrap_period
-    //                 .total_amount
-    //                 .fixed_mul_floor(remaining_time_ratio as i128, BPS_FACTOR)
-    //                 .map_over_or_underflow()?;
-    //             let diff = pool_bootstrap_period.remaining_amount - new_remaining_amount; // safe
-
-    //             let new_total_available =
-    //                 self.total_available.checked_add(diff).map_over_or_underflow()?;
-    //             self.total_available = new_total_available;
-
-    //             pool_bootstrap_period.remaining_amount = new_remaining_amount;
-    //             updated_periods.push_back(((start_period, end_period), pool_bootstrap_period));
-    //         }
-    //     }
-
-    //     for outdated_period in outdated_periods {
-    //         self.bootstrap_periods.remove(outdated_period);
-    //     }
-    //     for (period, updated_period) in updated_periods {
-    //         self.bootstrap_periods.set(period, updated_period);
-    //     }
-
-    //     Ok(())
-    // }
 
     /// Get current annual percentage yields (APY) for borrowing and supplying
     /// based on the pool's utilization ratio, interest rate model, and accrual model
