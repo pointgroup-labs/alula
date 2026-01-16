cb9543e0ab5278f5e1c14fd222a79da47c2baab3
garniy <forbedlam@gmail.com>
2026-01-16 12:44:31 +0200

    feat: enhance APY display component to include token symbols and improve additional APY calculation

--- packages/webapp/src/components/MarketsPage/Markets/Tables/MarketApyWithAdditional.vue
+++ packages/webapp/src/components/MarketsPage/Markets/Tables/MarketApyWithAdditional.vue
@@ -1,11 +1,11 @@
 /**
- * APY display component with optional additional incentives.
- *
- * - If `additionalMarketsData` contains incentive data for the current pool,
- *   the component shows a highlighted APY with a lightning icon.
- *   This value represents the **total APY**, including base pool APY
- *   and additional incentives.
- */
+* APY display component with optional additional incentives.
+*
+* - If `additionalMarketsData` contains incentive data for the current pool,
+* the component shows a highlighted APY with a lightning icon.
+* This value represents the **total APY**, including base pool APY
+* and additional incentives.
+*/
 <script lang="ts" setup>
 import type { MarketTableItem } from '~/types/table'
 
@@ -31,26 +31,25 @@ const additionalData = computed(() => {
   if (Object.keys(data).length === 0) {
     return null
   }
-  return data.find((item: any) => item.name === (isDeposit ? 'deposit' : 'borrow'))
+  return data.filter((item: any) => item.name === (isDeposit ? 'deposit' : 'borrow'))
 })
-
-const additionalApy = computed(() => additionalData.value?.additional_apy ?? 0)
 
 const poolApy = computed(() => isDeposit ? poolData?.deposit_apy : poolData?.borrow_apy)
 
 const apyWithAdittional = computed(() => {
-  if (!additionalData.value) {
+  if (!additionalData.value || additionalData.value.length === 0) {
     return 0
   }
   const poolApyNum = Number.parseFloat(poolApy.value ?? '0')
-  const totalApy = isDeposit ? poolApyNum + additionalApy.value : Math.max(poolApyNum - additionalApy.value, 0)
+  const additionalApy = additionalData.value.reduce((acc: typeof additionalData.value, item: typeof additionalData.value[number]) => acc + item.additional_apy, 0)
+  const totalApy = isDeposit ? poolApyNum + additionalApy : Math.max(poolApyNum - additionalApy, 0)
   return totalApy || 0
 })
 </script>
 
 <template>
   <lighting-apy
-    v-if="additionalData"
+    v-if="additionalData && additionalData.length > 0"
     :label="`${truncatePercent(apyWithAdittional, 2)}%`"
     :variant="isDeposit ? 'deposit' : 'borrow'"
   >
@@ -60,14 +59,22 @@ const apyWithAdittional = computed(() => {
           This position earns additional incentives:
         </div>
         <div class="separator" />
-        <div class="additional-tip__value">
-          Additional APY: <span :style="{ color: isDeposit ? '#08b576' : 'rgb(255, 165, 0)' }">{{ truncatePercent(additionalApy, 2) }}% </span>
+        <div
+          v-for="data in additionalData"
+          :key="data.additional_apy"
+          class="additional-tip__value"
+        >
+          {{ data.token_symbol }} REWARDS: <span :style="{ color: isDeposit ? '#08b576' : 'rgb(255, 165, 0)' }">
+            {{ truncatePercent(data.additional_apy, 2) }}% </span>
         </div>
         <div class="additional-tip__value">
-          {{ isDeposit ? 'Deposit' : 'Borrow' }} APY:<span>{{ poolApy }}</span>
+          {{ isDeposit ? 'Lending' : 'Borrow' }} APY:<span>{{ poolApy }}</span>
         </div>
+        
+        <div class="separator" />
+
         <div class="additional-tip__value">
-          Total APY:  <span>{{ truncatePercent(apyWithAdittional, 2) }}% </span>
+          Total Combined APY: <span>{{ truncatePercent(apyWithAdittional, 2) }}% </span>
         </div>
       </div>
     </template>
--- packages/webapp/src/hooks/additional-apy.ts
+++ packages/webapp/src/hooks/additional-apy.ts
@@ -1,3 +1,5 @@
+import type { TableAsset } from '~/types/table'
+
 export function useAdditionalApy() {
   const additionalMarketsData = ref<{ marketName: string, [key: string]: any }[]>([])
 
@@ -5,27 +7,34 @@ export function useAdditionalApy() {
     if (!markets) {
       return []
     }
+    const assetNames = [...new Set(markets.flatMap(m => m.assets.map((a: TableAsset['asset']) => a.symbol)))]
     additionalMarketsData.value = markets?.map((m) => {
-      const itemsData = m.tableItems.map((i: { pool_address: any }) => {
+      const itemsData = m.tableItems.map((item: { pool_address: any, raw?: any }) => {
         const randomNum = Math.round(Math.random())
         const randomDeposit = Math.round(Math.random())
         const randomBorrow = Math.round(Math.random())
         const data = []
         if (randomNum === 1) {
           if (randomDeposit === 1) {
-            data.push({
-              name: 'deposit',
-              additional_apy: Math.random() * 5,
-            })
+            for (let j = 0; j < Math.ceil(Math.random() * 3); j++) {
+              data.push({
+                name: 'deposit',
+                additional_apy: Math.random() * 3,
+                token_symbol: assetNames[Math.ceil(Math.random() * assetNames.length) - 1],
+              })
+            }
           }
           if (randomBorrow === 1) {
-            data.push({
-              name: 'borrow',
-              additional_apy: Math.random() * 3,
-            })
+            for (let k = 0; k < Math.ceil(Math.random() * 3); k++) {
+              data.push({
+                name: 'borrow',
+                additional_apy: Math.random() * 3,
+                token_symbol: assetNames[Math.ceil(Math.random() * assetNames.length) - 1],
+              })
+            }
           }
         }
-        return [i.pool_address, data]
+        return [item.pool_address, data]
       })
 
       return {
