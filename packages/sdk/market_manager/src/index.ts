3aa3b9a611096412d1ed523f22cf3bba9a7df42b
garniy <forbedlam@gmail.com>
2026-03-05 16:43:34 +0200

    fix: reorganize imports and improve code formatting in index.ts

--- packages/sdk/market_manager/src/index.ts
+++ packages/sdk/market_manager/src/index.ts
@@ -1,8 +1,15 @@
 import type {
+  Duration,
+  i32,
+  i64,
   i128,
+  i256,
   Option,
+  Timepoint,
   u32,
   u64,
+  u128,
+  u256,
 } from '@stellar/stellar-sdk/contract'
 import { Buffer } from 'node:buffer'
 import {
@@ -10,57 +17,41 @@ import {
   Client as ContractClient,
   ClientOptions as ContractClientOptions,
   Spec as ContractSpec,
+
   MethodOptions,
   Result,
-  Spec as ContractSpec,
-} from "@stellar/stellar-sdk/contract";
-import type {
-  u32,
-  i32,
-  u64,
-  i64,
-  u128,
-  i128,
-  u256,
-  i256,
-  Option,
-  Timepoint,
-  Duration,
-} from "@stellar/stellar-sdk/contract";
-export * from "@stellar/stellar-sdk";
-export * as contract from "@stellar/stellar-sdk/contract";
-export * as rpc from "@stellar/stellar-sdk/rpc";
+} from '@stellar/stellar-sdk/contract'
 
-if (typeof window !== "undefined") {
-  //@ts-ignore Buffer exists
-  window.Buffer = window.Buffer || Buffer;
+export * from '@stellar/stellar-sdk'
+export * as contract from '@stellar/stellar-sdk/contract'
+export * as rpc from '@stellar/stellar-sdk/rpc'
+
+if (typeof window !== 'undefined') {
+  // @ts-ignore Buffer exists
+  window.Buffer = window.Buffer || Buffer
 }
-
-
-
 
 /**
  * Market Manager Contract Error
  */
 export const MMCError = {
-  1: {message:"InvalidInputAmount"},
-  1000: {message:"MarketAlreadyExists"},
-  1001: {message:"InvalidMarketState"}
+  1: { message: 'InvalidInputAmount' },
+  1000: { message: 'MarketAlreadyExists' },
+  1001: { message: 'InvalidMarketState' },
 }
 
-export type DataKey = {tag: "Admin", values: void} | {tag: "MarketContractWasmHash", values: void} | {tag: "MarketList", values: void};
-
+export type DataKey = { tag: 'Admin', values: void } | { tag: 'MarketContractWasmHash', values: void } | { tag: 'MarketList', values: void }
 
 export interface Config {
-  admin: string;
-  market_contract_wasm_hash: Buffer;
+  admin: string
+  market_contract_wasm_hash: Buffer
 }
 
 export interface Client {
   /**
    * Construct and simulate a deploy transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
    */
-  deploy: ({salt, market_admin, name, oracle, swap_provider, insurance_fund, max_positions, min_collateral_value_cents, insolvency_ltv_bps, update_in_queue_period}: {salt: Buffer, market_admin: string, name: string, oracle: string, swap_provider: string, insurance_fund: string, max_positions: u32, min_collateral_value_cents: i128, insolvency_ltv_bps: i128, update_in_queue_period: Option<u64>}, options?: MethodOptions) => Promise<AssembledTransaction<Result<string>>>
+  deploy: ({ salt, market_admin, name, oracle, swap_provider, insurance_fund, max_positions, min_collateral_value_cents, insolvency_ltv_bps, update_in_queue_period}: { salt: Buffer, market_admin: string, name: string, oracle: string, swap_provider: string, insurance_fund: string, max_positions: u32, min_collateral_value_cents: i128, insolvency_ltv_bps: i128, update_in_queue_period: Option<u64> }, options?: MethodOptions) => Promise<AssembledTransaction<Result<string>>>
 
   /**
    * Construct and simulate a get_markets transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
@@ -75,31 +66,32 @@ export interface Client {
   /**
    * Construct and simulate a upgrade transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
    */
-  upgrade: ({new_wasm_hash}: {new_wasm_hash: Buffer}, options?: MethodOptions) => Promise<AssembledTransaction<null>>
+  upgrade: ({ new_wasm_hash}: { new_wasm_hash: Buffer }, options?: MethodOptions) => Promise<AssembledTransaction<null>>
 
   /**
    * Construct and simulate a upgrade_deployed_markets transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
    */
-  upgrade_deployed_markets: ({new_market_contract_wasm_hash}: {new_market_contract_wasm_hash: Buffer}, options?: MethodOptions) => Promise<AssembledTransaction<null>>
+  upgrade_deployed_markets: ({ new_market_contract_wasm_hash}: { new_market_contract_wasm_hash: Buffer }, options?: MethodOptions) => Promise<AssembledTransaction<null>>
 
 }
 export class Client extends ContractClient {
   static async deploy<T = Client>(
-        /** Constructor/Initialization Args for the contract's `__constructor` method */
-        {admin, market_contract_wasm_hash}: {admin: string, market_contract_wasm_hash: Buffer},
+    /** Constructor/Initialization Args for the contract's `__constructor` method */
+    { admin, market_contract_wasm_hash}: { admin: string, market_contract_wasm_hash: Buffer },
     /** Options for initializing a Client as well as for calling a method, with extras specific to deploying. */
-    options: MethodOptions &
-      Omit<ContractClientOptions, "contractId"> & {
+    options: MethodOptions
+      & Omit<ContractClientOptions, 'contractId'> & {
         /** The hash of the Wasm blob, which must already be installed on-chain. */
-        wasmHash: Buffer | string;
+        wasmHash: Buffer | string
         /** Salt used to generate the contract's ID. Passed through to {@link Operation.createCustomContract}. Default: random. */
-        salt?: Buffer | Uint8Array;
+        salt?: Buffer | Uint8Array
         /** The format used to decode `wasmHash`, if it's provided as a string. */
-        format?: "hex" | "base64";
-      }
+        format?: 'hex' | 'base64'
+      },
   ): Promise<AssembledTransaction<T>> {
-    return ContractClient.deploy({admin, market_contract_wasm_hash}, options)
+    return ContractClient.deploy({ admin, market_contract_wasm_hash }, options)
   }
+
   constructor(public readonly options: ContractClientOptions) {
     super(
       new ContractSpec(['AAAAAAAAAAAAAAAGZGVwbG95AAAAAAAJAAAAAAAAAARzYWx0AAAD7gAAACAAAAAAAAAADG1hcmtldF9hZG1pbgAAABMAAAAAAAAABG5hbWUAAAAQAAAAAAAAAAZvcmFjbGUAAAAAABMAAAAAAAAADmluc3VyYW5jZV9mdW5kAAAAAAATAAAAAAAAAA1tYXhfcG9zaXRpb25zAAAAAAAABAAAAAAAAAAObWluX2NvbGxhdGVyYWwAAAAAAAsAAAAAAAAAEmluc29sdmVuY3lfbHR2X2JwcwAAAAAACwAAAAAAAAAWdXBkYXRlX2luX3F1ZXVlX3BlcmlvZAAAAAAD6AAAAAYAAAABAAAD6QAAABMAAAfQAAAACE1NQ0Vycm9y',
@@ -114,11 +106,12 @@ export class Client extends ContractClient {
       options,
     )
   }
+
   public readonly fromJSON = {
     deploy: this.txFromJSON<Result<string>>,
-        get_markets: this.txFromJSON<Map<string, void>>,
-        get_config: this.txFromJSON<Config>,
-        upgrade: this.txFromJSON<null>,
-        upgrade_deployed_markets: this.txFromJSON<null>
+    get_markets: this.txFromJSON<Map<string, void>>,
+    get_config: this.txFromJSON<Config>,
+    upgrade: this.txFromJSON<null>,
+    upgrade_deployed_markets: this.txFromJSON<null>,
   }
 }
