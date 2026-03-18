e15521e4a799916f6822389382860adcb9b43fb5
garniy <forbedlam@gmail.com>
2026-03-18 17:38:47 +0200

    feat: replace NotValidAccountWarning component with inline warning-block in MarketsPage

--- packages/webapp/src/components/MarketsPage/MarketsPage.vue
+++ packages/webapp/src/components/MarketsPage/MarketsPage.vue
@@ -1,10 +1,16 @@
 <script lang="ts" setup>
 const { width } = useWindowSize()
+const store = useClientStore()
+const walletStore = useWallet()
+const isValidAccount = computed(() => store.isValidAccount && !!walletStore.publicKey)
 </script>
 
 <template>
   <main class="page-container container">
-    <not-valid-account-warning />
+    <warning-block
+      v-if="!isValidAccount && walletStore.publicKey"
+      text="The wallet address GA726GBW...SAWNEACK does not exist on the network. Please fund your account!"
+    />
     <markets-info v-if="width > 1024" />
     <markets />
   </main>
