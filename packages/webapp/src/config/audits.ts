e06fd29575383b2a755efbeef0ae223c1f54be43
garniy <forbedlam@gmail.com>
2026-04-07 16:08:33 +0300

    refactor: update Halborn auditor link to point to specific audit page

--- packages/webapp/src/config/audits.ts
+++ packages/webapp/src/config/audits.ts
@@ -2,7 +2,7 @@ import halbornLogo from '~/assets/img/auditor/halborn.webp'
 
 export const HALBORN_AUDITOR = {
   name: 'Halborn',
-  link: 'https://www.halborn.com/',
+  link: 'https://www.halborn.com/audits/alula-finance/smart-contracts-cd8f6d',
   logo: halbornLogo,
   auditedAt: '2026-03-24',
 }
