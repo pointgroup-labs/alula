131ff9e080191c1f78ace29047d80f255e4df93c
garniy <forbedlam@gmail.com>
2026-04-24 21:17:50 +0300

    feat: add summary list and select asset button styles in MultiplySupplyCard and WithdrawMultiplyWindow

--- packages/webapp/src/components/MultiplySupplyCard/WithdrawMultiplyWindow.vue
+++ packages/webapp/src/components/MultiplySupplyCard/WithdrawMultiplyWindow.vue
@@ -390,5 +390,93 @@ const closeDetailsSteps = computed(() => {
       transform: rotate(180deg);
     }
   }
+
+  .summary-list__item {
+    padding-bottom: 4px;
+    .label {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+
+      .step-id {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: $navi-100;
+        background-color: $navi-400;
+        border-radius: 50%;
+        width: 16px;
+        height: 16px;
+        font-size: 9px;
+        font-weight: 700;
+      }
+    }
+    .label-with-tip {
+      display: flex;
+      align-items: center;
+      gap: 6px;
+    }
+    .sub-label {
+      font-size: 11px;
+      color: rgb(79, 96, 128);
+      margin-left: 18px;
+
+      .line-arrow-icon {
+        width: 8px;
+        height: 12px;
+        margin-right: 4px;
+      }
+    }
+
+    &:last-child {
+      .sub-label {
+        margin-left: 24px;
+        svg {
+          display: none;
+        }
+      }
+    }
+  }
+
+  .select-asset-btn {
+    .asset-icons {
+      position: relative;
+      width: 24px;
+      height: 24px;
+
+      img {
+        position: absolute;
+        width: 18px;
+        height: 18px;
+
+        &:nth-child(1) {
+          left: 0;
+          top: 0;
+        }
+        &:nth-child(2) {
+          right: -2px;
+          bottom: -2px;
+        }
+      }
+    }
+
+    .swap-asset-label {
+      white-space: nowrap;
+      display: flex;
+      align-items: center;
+      gap: 4px;
+
+      svg {
+        width: 12px;
+        height: 12px;
+        color: #fff;
+      }
+
+      span {
+        font-size: 12px;
+        font-weight: 500;
+      }
+    }
+  }
 }
 </style>
